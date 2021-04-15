e421e8b8464c06a495420d571b80fd95cd66fff8
Megan Day <meganeday7@gmail.com>
2021-04-14 20:32:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let cards = [];
+let names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    };
+    return cards;
+};
+
+writeCards();
+
+function countDown() {
+    let count = 10;
+    while (count > -1) {
+        console.log(count--);
+    }
+};
